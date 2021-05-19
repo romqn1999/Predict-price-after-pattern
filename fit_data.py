21ea4800271ee753b1558a53836d90409ab5b7d0
dac.le <dac.le@eyeq.tech>
2021-05-19 23:45:37 +0700

    update fit_data.py

--- fit_data.py
+++ fit_data.py
@@ -10,7 +10,7 @@ def polyfit(x_data, y_data, degree=80):
     return y_pol
 
 
-def gaussfit(x_data, y_data, max_len_per_fit=200):
+def gaussfit(x_data, y_data, chunk_size=200):
     # Define kernel parameters. 
     l = 0.1
     sigma_f = 2
@@ -23,8 +23,15 @@ def gaussfit(x_data, y_data, max_len_per_fit=200):
 
     # Define GaussianProcessRegressor object. 
     gp = GaussianProcessRegressor(kernel=kernel, alpha=sigma_n**2, n_restarts_optimizer=2)
-    # Fit to data using Maximum Likelihood Estimation of the parameters
-    gp.fit(np.array(x_data).reshape(-1, 1), y_data)
-    # Make the prediction on the meshed x-axis (ask for MSE as well)
-    y_pol, sigma = gp.predict(np.array(x_data).reshape(-1, 1), return_std=True)
-    return y_pol
+
+    y_res = np.empty((0))
+    # Split data into sized chunks
+    for i in range(0, len(x_data), chunk_size):
+        x_chunk = x_data[i: i + chunk_size]
+        y_chunk = y_data[i: i + chunk_size]
+        # Fit to data using Maximum Likelihood Estimation of the parameters
+        gp.fit(np.array(x_chunk).reshape(-1, 1), y_chunk)
+        # Make the prediction on the meshed x-axis (ask for MSE as well)
+        y_pre, sigma = gp.predict(np.array(x_chunk).reshape(-1, 1), return_std=True)
+        y_res.append(y_pre)
+    return y_res
